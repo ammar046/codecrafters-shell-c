743ba39ed4589a8e90ae9119ca2d4333180d65c2
ammar046 <ammarbinomer17@gmail.com>
2026-03-12 14:54:17 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+char *find_in_path(char *command)
+{
+  char *path_env = getenv("PATH");
+  if (!path_env)
+    return NULL;
+
+  char *path_copy = strdup(path_env);
+  char *dir = strtok(path_copy, ":");
+  static char result_path[1024];
+  int found = 0;
+
+  while (dir != NULL)
+  {
+    snprintf(result_path, sizeof(result_path), "%s/%s", dir, command);
+    if (access(result_path, X_OK) == 0)
+    {
+      found = 1;
+      break; // Exit loop so we can free path_copy
+    }
+    dir = strtok(NULL, ":");
+  }
+
+  free(path_copy); // Always free the copy
+  return found ? result_path : NULL;
+}
 
 int main(int argc, char *argv[])
 {
@@ -27,7 +54,15 @@ int main(int argc, char *argv[])
       }
       else
       {
-        printf("%s: not found\n", input + 5);
+        char *path_found = find_in_path(input + 5);
+        if (path_found)
+        {
+          printf("%s is %s\n", input + 5, path_found);
+        }
+        else
+        {
+          printf("%s: not found\n", input + 5);
+        }
       }
     }
     else
