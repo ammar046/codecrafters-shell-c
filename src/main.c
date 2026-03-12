ff3870740cc259ccbb4fd7252afe3bf59ab3a9f1
ammar046 <ammarbinomer17@gmail.com>
2026-03-12 14:17:46 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
   while (fgets(input, sizeof(input), stdin))
   {
     input[strcspn(input, "\n")] = 0;
+    if (strcmp(input, "exit") == 0)
+      break;
     printf("%s: command not found\n", input);
     printf("$ ");
   }
